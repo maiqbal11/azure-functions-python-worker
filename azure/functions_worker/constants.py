67d96507501eaae6f477708ae331cf3eeb741e8e
Maheer Iqbal <42051041+maiqbal11@users.noreply.github.com>
2019-06-25 15:44:48 -0700

    Remove absolute path syntax

--- azure/functions_worker/constants.py
+++ azure/functions_worker/constants.py
@@ -3,6 +3,6 @@ RAW_HTTP_BODY_BYTES = "RawHttpBodyBytes"
 
 # User packages
 USER_PKGS = [
-    '/site/wwwroot/.python_packages/lib/python3.6/site-packages',
-    '/site/wwwroot/worker_venv/lib/python3.6/site-packages'
+    'site/wwwroot/.python_packages/lib/python3.6/site-packages',
+    'site/wwwroot/worker_venv/lib/python3.6/site-packages'
 ]
